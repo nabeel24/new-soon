b3873ad43d2c150f81e398cbae5e9821b292f90e
nabeel24 <mqrshie@gmail.com>
2020-06-29 20:30:09 +0530

    My first commit

---
+++ basics.py
@@ -0,0 +1,8 @@
+temp = [45.1,25.2,35.25] 
+# students_grades = {"mary":45.1,"john":25.2,"smith":35.25}
+# mysum = sum(students_grades.values())
+# print(mysum)
+# print(students_grades.keys())
+# print(len(students_grades))
+temp.append(45)
+print(temp)
